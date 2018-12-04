c3d1b25aacba5463f15bf3ac8d5a75f0be47d961
Vatsaev Aslan <a.vatsaev@gmail.com>
2018-12-04 16:58:33 +0100

    fix: init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+interface IConfig {
+  columns: Array<{
+    visible: boolean,
+    size: number,
+    rows: Array<{
+      visible: boolean,
+      size?: number,
+      type: string
+    }>
+  }>;
+  disabled: boolean;
+}
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'split-screen-test';
+export class AppComponent implements OnInit {
+  config: IConfig = {
+    columns: [
+      {
+        visible: true,
+        size: 100,
+        rows: [
+          { visible: true, size: 50, type: 'A' },
+          { visible: true, size: 50, type: 'B' }
+        ]
+      }
+    ],
+    disabled: false
+  };
+
+  ngOnInit() {
+
+  }
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { AngularSplitModule } from 'angular-split';
+import { CompAComponent } from './comp-a/comp-a.component';
+import { CompBComponent } from './comp-b/comp-b.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompAComponent,
+    CompBComponent
   ],
   imports: [
+    AngularSplitModule,
     BrowserModule
   ],
   providers: [],
+  entryComponents: [CompAComponent, CompBComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
